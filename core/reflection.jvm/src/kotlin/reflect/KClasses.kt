49a92dc98a672f87910021c167e621352e0bbc4a
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2016-07-12 18:48:37 +0300

    (TODO) Add KClass.isInstance, KClass.cast, KClass.safeCast

--- core/reflection.jvm/src/kotlin/reflect/KClasses.kt
+++ core/reflection.jvm/src/kotlin/reflect/KClasses.kt
@@ -233,3 +233,24 @@ fun KType.isSubtypeOf(other: KType): Boolean {
 fun KType.isSupertypeOf(other: KType): Boolean {
     return other.isSubtypeOf(this)
 }
+
+
+/**
+ * Performs an unchecked cast of the given [value] to this class and either returns the value if the cast is successful,
+ * or throws an exception otherwise.
+ *
+ */
+@Suppress("UNCHECKED_CAST")
+fun <T : Any> KClass<T>.cast(value: Any?): T {
+    if (!isInstance(value)) throw TypeCastException("Value cannot be cast to $qualifiedName")
+    return value as T
+}
+
+/**
+ * Performs an unchecked cast of the given [value] to this class and returns either the value if the cast is successful,
+ * or `null` otherwise.
+ */
+@Suppress("UNCHECKED_CAST")
+fun <T : Any> KClass<T>.safeCast(value: Any?): T? {
+    return if (isInstance(value)) value as T else null
+}
